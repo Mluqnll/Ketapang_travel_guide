b1429da10ec1550eeffba20348f9f785daa8194f
Muhammad Luqinul Mifdlol Assiddiqi <101262825+Mluqnll@users.noreply.github.com>
2023-03-26 14:07:04 +0700

    update

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -9,37 +9,6 @@
                 <x-layout.sidebar.menu-item label="Atrakasi Wisata" icon="ion ion-ios-copy"/>
                 <x-layout.sidebar.menu-item label="Kalender Wisata" icon="ion ion-md-calendar"/>
                 <x-layout.sidebar.menu-item label="Kontak" icon="ion ion-md-call"/>
-                <li class="nav-item">
-                    <a class="nav-link link-with-badge" href="javascript:void(0);" data-toggle="collapse" data-target="#app_drp">
-                        <i class="ion ion-ios-person-add"></i>
-                        <span class="nav-link-text">Pengelola</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-with-badge" href="javascript:void(0);" data-toggle="collapse" data-target="#auth_drp">
-                        <i class=" ion ion-md-globe"></i>
-                        <span class="nav-link-text">Desa Wisata</span>
-                    </a>
-
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-with-badge" href="javascript:void(0);" data-toggle="collapse" data-target="#pages_drp">
-                        <i class="ion ion-ios-copy"></i>
-                        <span class="nav-link-text">Atraksi Wisata</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-with-badge" href="javascript:void(0);" data-toggle="collapse" data-target="#pages_drp">
-                        <i class="ion ion-md-calendar"></i>
-                        <span class="nav-link-text">Kalender Wisata</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-with-badge" href="javascript:void(0);" data-toggle="collapse" data-target="#pages_drp">
-                        <i class="ion ion-md-call"></i>
-                        <span class="nav-link-text">Kontak</span>
-                    </a>
-                </li>
             </ul>
         </div>
     </div>
